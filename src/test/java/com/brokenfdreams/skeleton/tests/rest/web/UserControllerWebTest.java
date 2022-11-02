2c65f1f6eb7de19afc84f9869daef0196fc55066
brokenfdreams <brokenFdreams@protonmail.com>
2022-11-02 17:37:47 +0400

    disable liquibase

--- src/test/java/com/brokenfdreams/skeleton/tests/rest/web/UserControllerWebTest.java
+++ src/test/java/com/brokenfdreams/skeleton/tests/rest/web/UserControllerWebTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Sql(
         executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-        scripts = {"/scripts/user-controller-web-data.sql"}
+        scripts = {"/scripts/schema.sql", "/scripts/user-controller-web-data.sql"}
 )
 @SpringBootTest
 @AutoConfigureMockMvc
